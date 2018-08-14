ada457f2c6c5c0cdbfd3b396b48222fb0c634b8b
juan_esteban.garcia <juan_esteban.garcia@SCG-05V-D58729.uao.edu.co>
2018-08-14 12:17:35 -0500

    Se crea el metodo para guardar archivos XML, ademas se modifica la interfaz grafica y la clase estudiante

--- src/creacionarchivoxml/FXMLDocumentController.java
+++ src/creacionarchivoxml/FXMLDocumentController.java
@@ -6,11 +6,14 @@
 package creacionarchivoxml;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import modelo.Estudiante;
 
 /**
  *
@@ -18,18 +21,61 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+   
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField nombreEstdudiante;
+    
+    @FXML
+    private TextField telefono; 
+    
+    @FXML
+    private TextField correo; 
+    
+    @FXML
+    private TextField codigo; 
+    
+    @FXML
+    private TextField carrera; 
+    
+    LinkedList<Estudiante> ListaE;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent e){
+     
+    
+    }
+    
+    @FXML
+    private void agregarEstdudiante (ActionEvent e){
+        
+         String nombre = nombreEstdudiante.getText();
+         String telefonoE = telefono.getText();
+         String correoE = correo.getText();
+         String codigoE = codigo.getText();
+         String carreraE = carrera.getText();
+         
+         Estudiante objE = new Estudiante(codigoE, carreraE, nombre, telefonoE, correoE);
+         ListaE.add(objE);
+        
+        
+        
+        
+    }
+    
+    @FXML
+    private void guardarArchivo(ActionEvent e){
+        
+        Estudiante objeV = new Estudiante();
+        boolean guardar = objeV.crearArchivoXML(ListaE);
+    
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        ListaE = new LinkedList<>();
     }    
     
 }
--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -5,20 +5,44 @@
  */
 package modelo;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 /**
  *
  * @author juan_esteban.garcia
  */
 public class Estudiante extends Persona{
     
+     
     private String codigo;
     private String carrera;
+    
+    /**
+     *
+     */
+    public Estudiante(){
+    
+    }
 
+        
+    
     public Estudiante(String codigo, String carrera, String nombre, String telefono, String correo) {
         super(nombre, telefono, correo);
         this.codigo = codigo;
         this.carrera = carrera;
     }
+
+    
+   
+    
+   
+
+   
     
     
 
@@ -41,6 +65,57 @@ public class Estudiante extends Persona{
     @Override
     public String toString() {
         return "Estudiante = {" + super.toString() + "codigo=" + codigo + ", carrera=" + carrera ;
+    }
+
+    public boolean crearArchivoXML(LinkedList<Estudiante> ListaE) {
+        
+        boolean g = false;
+                
+                
+       try{
+           
+           Element universidad = new Element ("estudiante"); //elemento = nueva etiqueta
+           Document doc = new Document(universidad);
+       
+           for (int i=0; i < ListaE.size(); i++){
+               
+               Element estudiante = new Element("estudiante");
+               estudiante.addContent(new Element("nombre").setText(
+                    ListaE.get(i).getNombre()));
+               
+               estudiante.addContent(new Element("telefono").setText(
+                    ListaE.get(i).getTelefono()));
+               
+               estudiante.addContent(new Element("correo").setText(
+                    ListaE.get(i).getCorreo()));
+               
+               estudiante.addContent(new Element("codigo").setText(
+                    ListaE.get(i).getCodigo()));
+              
+               estudiante.addContent(new Element("carrera").setText(
+                    ListaE.get(i).getCarrera()));
+               
+               
+               doc.getRootElement().addContent(estudiante); // añade y cierra un objetivo para almacenar los datos e impide que otro usuario edite datos
+                
+           
+           }
+           
+           XMLOutputter xmlOutput = new XMLOutputter();
+           xmlOutput.setFormat(Format.getPrettyFormat());
+           xmlOutput.output(doc, new FileWriter("universidad.xml"));
+           g =true;
+       
+       } catch(IOException e){
+           
+           
+           System.out.println(e.getMessage());
+           g = false;
+       }       
+                            
+                       
+                
+       return g;
     }
     
     
